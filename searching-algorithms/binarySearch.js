1ab5f2343e43de39a48779a2083be5f699fa29fd
Pavanarathod <edrocullen13@gmail.com>
2022-08-15 19:25:50 +0530

    this works

--- searching-algorithms/binarySearch.js
+++ searching-algorithms/binarySearch.js
@@ -1,24 +1,11 @@
-// TODO: UDNERSTAND BINARY SEARCH
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
 
-/**
- * * This function accepts a sorted array of values
- * * Create a left pinter at the start of the array
- * * Create a right pinter at the end of the array
- * * While the left pointer comes before the right pointer:
- * *      * Create a pinter at the middle
- * *      * if you find the value you want, return the index
- * *      * if the value is too small move the left pinter up
- * *      * If the value is too large, move the right pointer down
- * * if you never find the vlaue return -1
- */
-
-function binarySearch(arr, searchValue) {
-  var start = 0;
-  var end = arr.length - 1;
-  var middle = Math.floor((start + end) / 2);
-
-  while (arr[middle] !== searchValue && start <= end) {
-    if (searchValue < arr[middle]) {
+  while (arr[middle] !== target && start <= end) {
+    console.log("index", start, middle, end);
+    if (target < arr[middle]) {
       end = middle - 1;
     } else {
       start = middle + 1;
@@ -26,37 +13,56 @@ function binarySearch(arr, searchValue) {
     middle = Math.floor((start + end) / 2);
   }
 
-
-  if (arr[middle] === searchValue) {
+  //   console.log("index", start, middle, end);
+  if (arr[middle] === target) {
     return middle;
   }
-
   return -1;
 }
 
+const state = [
+  "Andhra Pradesh",
+  "Arunachal Pradesh",
+  "Assam",
+  "Bihar",
+  "Chhattisgarh",
+  "Goa",
+  "Gujarat",
+  "Haryana",
+  "Himachal Pradesh",
+  "Jammu and Kashmir",
+  "Jharkhand",
+  "Karnataka",
+  "Kerala",
+  "Madhya Pradesh",
+  "Maharashtra",
+  "Manipur",
+  "Meghalaya",
+  "Mizoram",
+  "Nagaland",
+  "Odisha",
+  "Punjab",
+  "Rajasthan",
+  "Sikkim",
+  "Tamil Nadu",
+  "Telangana",
+  "Tripura",
+  "Uttarakhand",
+  "Uttar Pradesh",
+  "West Bengal",
+  "Andaman and Nicobar Islands",
+  "Chandigarh",
+  "Dadra and Nagar Haveli",
+  "Daman and Diu",
+  "Delhi",
+  "Lakshadweep",
+  "Puducherry",
+];
 
-function binary(arr = [], target){
-    if(arr.length === 0){
-        
-        return
-    }
+binarySearch(state, "Uttarakhand");
 
-    let start  = 0;
-    let end = arr.length -1;
-    let middle = Math.floor((start + end) / 2)
-    
-    console.log({
-        start,
-        end,
-        middle
-    })
+// [2,5,6,9,13,15,28,30]
+//  S     M           E
 
-    if (arr[middle] === target) {
-        return middle;
-    } 
-
-   
-    
-}
-
-binary([2,3,5,7,8,9, 10, 15, 16], 15)
+// [2,5,6,9,13,15,28,30]
+//           S  M     E
