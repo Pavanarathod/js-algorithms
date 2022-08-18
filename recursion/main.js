adf4a87d9e778043f8d5064b5dff9e0e544c46f9
Pavanarathod <edrocullen13@gmail.com>
2022-08-18 12:27:56 +0530

    more recursion

--- recursion/main.js
+++ recursion/main.js
@@ -1,12 +1,3 @@
-// TODO: Understand recursion and it's advantages
-
-/**
- * * A process that calls itself
- * * A function calls itself
- * * Base case
- * * Different input cases
- */
-
 function countDown(num) {
   if (num <= 0) {
     console.log("All done");
@@ -14,14 +5,23 @@ function countDown(num) {
   }
 
   console.log(num);
-  num -= 1;
+  num = num - 1;
   countDown(num);
 }
 
+// countDown(3)
+
 function sumRange(num) {
-  if (num === 1) return 1;
+  if (num === 1) {
+    console.log(num);
+    return 1;
+  }
+
+  console.log(num);
   return num + sumRange(num - 1);
 }
+
+// sumRange(4)
 
 function factorial(num) {
   let total = 1;
@@ -31,48 +31,20 @@ function factorial(num) {
   return total;
 }
 
+// factorial(4)
+
 function recursiveFactorial(num) {
+  if (num === 1) return 1;
   return num * recursiveFactorial(num - 1);
 }
+/**
+recursiveFactorial(6)
+  return 6 * recursiveFactorial(5)
+         return 5 * recursiveFactorial(4)
+                return 4 * recursiveFactorial(3)
+                       return 3 * recursiveFactorial(2)
+                              return 2 * recursiveFactorial(1)
+                                     return 1;
+*/
 
-// recursive helper uproch.
-
-function collectOddValues(arr) {
-  let result = [];
-
-  function helper(helperInput = []) {
-    if (helperInput.length === 0) {
-      return;
-    }
-
-    console.log(helperInput[0]);
-    if (helperInput[0] % 2 !== 0) {
-      result.push(helperInput[0]);
-    }
-    helper(helperInput.slice(1));
-  }
-
-  helper(arr);
-  return result;
-}
-
-function collectOddValues(arr) {
-  let newArr = [];
-
-  if (arr.length === 0) {
-    return newArr;
-  }
-
-  console.log(arr[0] % 2);
-  if (arr[0] % 2 !== 0) {
-    console.log(arr[0] % 2);
-    newArr.push(arr[0]);
-  }
-
-  newArr = newArr.concat(collectOddValues(arr.slice(1)));
-  return newArr;
-}
-
-collectOddValues([1, 2, 3, 4, 5]);
-
-countDown(5);
+recursiveFactorial(5);
