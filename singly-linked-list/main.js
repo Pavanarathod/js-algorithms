2d9fa9ed069228ee085c05a2f5e993a9a5e461c2
Pavanarathod <edrocullen13@gmail.com>
2022-08-20 20:25:16 +0530

    continue singly linked list

--- singly-linked-list/main.js
+++ singly-linked-list/main.js
@@ -25,6 +25,39 @@ class SinglyLinkedList {
     return this;
   }
 
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+
+    console.log("current before loop", current);
+    console.log("new tail before loop", newTail);
+
+    console.log("******************Loop begins***********");
+
+    let loopCount = 1;
+    while (current.next) {
+      console.log("*****Loop count******", loopCount);
+      console.log("current", current);
+      console.log("new tail", newTail);
+      console.log("our next current will be this node", current.next);
+      // previos node not the next node;
+      loopCount++;
+      newTail = current;
+      current = current.next;
+    }
+
+    console.log("******************Loop ends***********");
+
+    console.log("current after loop", current);
+    console.log("new tail after loop", newTail);
+
+    this.tail = newTail;
+    this.length--;
+    this.tail.next = null;
+    return current;
+  }
+
   traverse() {
     let current = this.head;
     while (current) {
@@ -35,12 +68,7 @@ class SinglyLinkedList {
 }
 
 const list = new SinglyLinkedList();
-list.push("pavan");
-list.push("Abhishek");
-list.push("Manjunath");
-
-// const first = new Node("Hello");
-// first.next = new Node("Friend");
-// first.next.next = new Node("fuck");
-// first.next.next.next = new Node("You");
-// console.log(JSON.stringify(first, null, 4));
+list.push("React");
+list.push("Anguler");
+list.push("Vue");
+list.push("Jquery");
