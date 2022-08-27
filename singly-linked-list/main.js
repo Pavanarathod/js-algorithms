1906b4912f227647c20dc100c957198cb1ae15d6
Pavanarathod <edrocullen13@gmail.com>
2022-08-27 21:37:57 +0530

    stuck with linked list

--- singly-linked-list/main.js
+++ singly-linked-list/main.js
@@ -50,7 +50,60 @@ class SinglyLinkedList {
     let currentHead = this.head;
     this.head = currentHead.next;
     this.length -= 1;
+    if (this.length === 0) {
+      this.tail = null;
+    }
     return currentHead;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length += 1;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+
+    return current;
+  }
+
+  set(index, value) {
+    const foundNode = this.get(index);
+    if (!foundNode) return false;
+    foundNode.val = value;
+    return true;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(value);
+    if (index === 0) return !!this.unshift(value);
+
+    const newNode = new Node(value);
+
+    const previousNode = this.get(index - 1);
+    const temp = previousNode.next;
+    previousNode.next = newNode;
+    newNode.next = temp;
+
+    this.length++;
+    return true;
   }
 
   listTraverse() {
