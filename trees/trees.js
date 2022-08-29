653de2060b1a88a0e49f488aa42dc117391af3cb
Pavanarathod <edrocullen13@gmail.com>
2022-08-29 22:16:19 +0530

    search node in tree

--- trees/trees.js
+++ trees/trees.js
@@ -41,6 +41,56 @@ class BST {
       }
     }
   }
+
+  //* Finding a node in the tree..
+
+  find(value) {
+    if (this.root === null) return false;
+    if (this.root.value === value) return true;
+    else {
+      let current = this.root;
+      let found = false;
+
+      while (current && !found) {
+        if (value < current.value) {
+          current = current.left;
+        } else if (value > current.value) {
+          current = current.right;
+        } else {
+          return true;
+        }
+      }
+
+      if (!found) return false;
+      return current;
+    }
+  }
+
+  search(value) {
+    if (this.root === null) return false;
+    if (this.root.value === value) return true;
+    else {
+      let current = this.root;
+
+      while (current && !found) {
+        if (value < current.value) {
+          current = current.left;
+        } else if (value > current.value) {
+          current = current.right;
+        } else {
+          found = true;
+        }
+      }
+
+      return current ? current : "not found";
+    }
+  }
 }
 
 const tree = new BST();
+tree.insert(10);
+tree.insert(4);
+tree.insert(2);
+tree.insert(12);
+tree.insert(15);
+tree.insert(20);
